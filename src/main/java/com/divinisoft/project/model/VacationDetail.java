71600ee8cb75dceee99d2f66b854f4be14b11fbf
Meena Tudu <meenawati.tudu@gmail.com>
2020-03-20 14:00:37 +0530

    added model classes

---
+++ src/main/java/com/divinisoft/project/model/VacationDetail.java
@@ -0,0 +1,25 @@
+package com.divinisoft.project.model;
+
+import java.util.Date;
+
+public class VacationDetail {
+	
+	private String vacationType;
+	private Date date;
+	
+	public String getVacationType() {
+		return vacationType;
+	}
+	public void setVacationType(String vacationType) {
+		this.vacationType = vacationType;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+
+}
